25fc77b30accc98b3ebe26dd2bd6ef87e7c3f961
Himujjal Upadhyaya <coolhimujjal@hotmail.com>
2018-04-15 18:47:01 +0530

    Update c-run.bat

--- c-run.bat
+++ c-run.bat
@@ -3,7 +3,7 @@ set filename=%1
 set filename2=%2
 cls
 if /I "%filename2%"=="" (
-    nodemon --watch %filename%.c --exec "cls && gcc -w %filename%.c -o %filename%.exe && %filename%.exe"
+    nodemon --watch %filename%.c -e c --exec "cls && gcc -w %filename%.c -o %filename%.exe && %filename%.exe"
 ) ELSE (
-    nodemon --watch %filename2%.c --watch %filename%.c --watch %filename2%.h --exec "cls && gcc -w %filename2%.c %filename%.c -o %filename%.exe && %filename%.exe"
+    nodemon --watch %filename2%.c -e c --watch %filename%.c --watch %filename2%.h --exec "cls && gcc -w %filename2%.c %filename%.c -o %filename%.exe && %filename%.exe"
 )
